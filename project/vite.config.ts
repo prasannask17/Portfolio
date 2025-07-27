a703ad3db6b974ddc12f7415a70dd30c041cbd57
Prasanna SA <prasannask17@gmail.com>
2025-07-27 23:09:56 +0530

    Add build config to vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,9 +1,23 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
